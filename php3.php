9a1e761fba41a6574277fb30dda9a3c0c292e162
190103425 <190103426@stu.sdu.edu.kz>
2021-02-08 05:56:18 +0600

    commit4

---
+++ php3.php
@@ -0,0 +1,18 @@
+<?php
+$color = "yellow";
+
+switch ($color){
+    case "blue":
+        echo "My favorite color is blue!";
+        break;
+    case "red":
+        echo "My favorite color is red!";
+        break;  
+    case "yellow":
+        echo "My favorite color is yellow!";
+        break;  
+    default:
+        echo "My favorite color is neither blue,red, nor yellow!";
+
+}
+?>
